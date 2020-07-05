93b578d7f1f0ef5b0ec72f1a37d240d91f8181e8
Wesley Rocha <wesleysr1997@gmail.com>
2020-07-05 19:07:36 -0300

    break for real now

--- weasel.py
+++ weasel.py
@@ -7,49 +7,7 @@ import click
 VALID_CHARSET = string.ascii_uppercase + ' '
 
 
-def validate_target(ctx, param, value):
+dxxef validate_target(ctx, param, value):
     if set(value).union(VALID_CHARSET) != set(VALID_CHARSET):
         raise click.BadParameter('Target string must only contain spaces and uppercase ASCII characters.')
     return value
-
-
-@click.command()
-@click.option(
-    '-t', '--target',
-    type=str,
-    default='METHINKS IT IS LIKE A WEASEL',
-    callor/--no-color', 'is_colorized',
-    default=True,
-    is_flag=True,
-    show_default=True,
-    help='Uses ANSI colors when reporting generation results')
-def cli(target, population_size, mutation_rate, is_colorized):
-    """Simulate Dawkins' weasel experiment"""
-    best_subject = ''.join(random.choices(VALID_CHARSET, k=len(target)))
-    generation = 0
-
-    def mutate(subject):
-        return ''.join(
-            c if random.random() >= mutation_rate
-            else random.choice(VALID_CHARSET) for c in subject)
-
-    def evaluate(subject):
-        return sum(a == b for a, b in zip(subject, target))
-
-    def report(subject):
-        click.echo(f'{generation: 6}: ', nl=False)
-        styled_subject = ''.join(
-            click.style(a, bold=True, fg='green' if a == b else 'red')
-            for a, b in zip(subject, target))
-        click.echo(styled_subject, color=is_colorized, nl=False)
-        score = evaluate(subject)
-        click.echo(f' -- score: {score}/{len(target)}')
-
-    while True:
-        population = population_size * [best_subject]
-        population = map(mutate, population)
-        best_subject = max(population, key=evaluate)
-        report(best_subject)
-        if best_subject == target:
-            return 0
-        generation += 1
